ace0cbd18bad4fbbeff275e84d8bccbfd1943c8b
Ephraim Kunz <ephraimkunz@me.com>
2024-09-11 15:42:09 -0600

    Initial Commit

---
+++ Visionary/VisionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionaryApp.swift
+//  Visionary
+//
+//  Created by Ephraim Kunz on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
